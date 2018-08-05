f9d43d97ae36d26b74e91742d823825af96f4716
Lorenzo Gangemi <Lorenzo.Gangemi.Developer@gmail.com>
2018-08-05 23:46:04 +0200

    Create project in Studio. Create library for Codice fiscale.

---
+++ codicefiscaleutil/src/main/java/gangemi/lorenzo/codicefiscaleutil/Builder.java
@@ -0,0 +1,12 @@
+package gangemi.lorenzo.codicefiscaleutil;
+
+import android.util.Log;
+
+public class Builder {
+
+    private static final String TAG = "CF_BUILDER";
+
+    public static void d(String message) {
+        Log.d(TAG,message);
+    }
+}
