6f9a65ba51ddd421a6c088f27d1985701d6e35fd
Lorenzo Gangemi <Lorenzo.Gangemi.Developer@gmail.com>
2018-08-06 00:23:25 +0200

    06/08/2018

---
+++ codicefiscaleutil/src/main/java/gangemi/lorenzo/codicefiscaleutil/CF_Builder.java
@@ -0,0 +1,21 @@
+package gangemi.lorenzo.codicefiscaleutil;
+
+import android.util.Log;
+
+public class Builder {
+
+    private static final String TAG = "CF_BUILDER";
+
+    private static void e(String message) {
+        Log.e(TAG,message);
+    }
+
+    private static void d(String message) {
+        Log.d(TAG,message);
+    }
+
+
+    public static String build() {
+        return "Codice Fiscale";
+    }
+}
